74af49e8e243a5c587883de2ebb0032ad11709cf
Agus Sumarna <sumarna.agus@gmail.com>
2019-10-10 13:27:53 +0700

    logo

--- app/src/main/java/xyz/imtiyaz/splashscreen/ActivityHome.java
+++ app/src/main/java/xyz/imtiyaz/splashscreen/ActivityHome.java
@@ -1,31 +1,12 @@
 package xyz.imtiyaz.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class ActivityHome extends AppCompatActivity {
-
-    Button bt_click_me;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        //Add all your codes here
-        bt_click_me = findViewById(R.id.bt_click_me);
-        bt_click_me.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityHome.this, "You clicked me. Thank you!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //Happy coding :)
-
     }
 }
--- app/src/main/java/xyz/imtiyaz/splashscreen/ActivitySplash.java
+++ app/src/main/java/xyz/imtiyaz/splashscreen/ActivitySplash.java
@@ -9,36 +9,29 @@ import android.os.Handler;
 
 public class ActivitySplash extends AppCompatActivity {
     ProgressBar splashProgress;
-    int SPLASH_TIME = 3000; //This is 3 seconds
+    int SPLASH_TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //This is additional feature, used to run a progress bar
         splashProgress = findViewById(R.id.splashProgress);
         playProgress();
 
-        //Code to start timer and take action after the timer ends
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //Do any action here. Now we are moving to next page
                 Intent mySuperIntent = new Intent(ActivitySplash.this, ActivityHome.class);
                 startActivity(mySuperIntent);
-
-                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
                 finish();
-
             }
         }, SPLASH_TIME);
     }
 
-    //Method to run progress bar for 5 seconds
     private void playProgress() {
         ObjectAnimator.ofInt(splashProgress, "progress", 100)
-                .setDuration(5000)
-                .start();
+            .setDuration(5000)
+            .start();
     }
 }
