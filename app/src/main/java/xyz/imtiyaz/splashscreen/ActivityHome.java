9405dcf8d0ddfd8336f97b48d046597a7f28e0dc
Agus Sumarna <sumarna.agus@gmail.com>
2019-10-10 11:04:43 +0700

    logo

--- app/src/main/java/xyz/imtiyaz/splashscreen/ActivityHome.java
+++ app/src/main/java/xyz/imtiyaz/splashscreen/ActivityHome.java
@@ -7,21 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityHome extends AppCompatActivity {
 
     Button bt_click_me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         //Add all your codes here
         bt_click_me = findViewById(R.id.bt_click_me);
         bt_click_me.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You clicked me. Thank you!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ActivityHome.this, "You clicked me. Thank you!", Toast.LENGTH_SHORT).show();
             }
         });
 
--- app/src/main/java/xyz/imtiyaz/splashscreen/ActivitySplash.java
+++ app/src/main/java/xyz/imtiyaz/splashscreen/ActivitySplash.java
@@ -1,14 +1,13 @@
 package xyz.imtiyaz.splashscreen;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.animation.ObjectAnimator;
+import android.widget.ProgressBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ProgressBar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SplashActivity extends AppCompatActivity {
+public class ActivitySplash extends AppCompatActivity {
     ProgressBar splashProgress;
     int SPLASH_TIME = 3000; //This is 3 seconds
 
@@ -26,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //Do any action here. Now we are moving to next page
-                Intent mySuperIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent mySuperIntent = new Intent(ActivitySplash.this, ActivityHome.class);
                 startActivity(mySuperIntent);
 
                 //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
